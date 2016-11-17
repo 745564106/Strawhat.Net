9992663feb3f53d6185514db807b3eb47cf1df48
Muhammad Fadhlika <fadhlika@gmail.com>
2016-11-17 23:21:33 +0700

    initial commit

--- Strawhat.net/Form1.Designer.cs
+++ Strawhat.net/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Strawhat.net
 {
-    partial class Form1
+    partial class MonitorWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,235 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.terminalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.plotToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.portBox = new System.Windows.Forms.ComboBox();
+            this.baudBox = new System.Windows.Forms.ComboBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.sendBox = new System.Windows.Forms.ComboBox();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.receiveText = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.menu, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.portBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.baudBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.connectButton, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.sendBox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.sendButton, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.receiveText, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 5);
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(409, 311);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // menu
+            // 
+            this.menu.BackColor = System.Drawing.SystemColors.Control;
+            this.tableLayoutPanel1.SetColumnSpan(this.menu, 4);
+            this.menu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.terminalToolStripMenuItem,
+            this.plotToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(409, 23);
+            this.menu.TabIndex = 0;
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // terminalToolStripMenuItem
+            // 
+            this.terminalToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem,
+            this.refreshToolStripMenuItem});
+            this.terminalToolStripMenuItem.Name = "terminalToolStripMenuItem";
+            this.terminalToolStripMenuItem.Size = new System.Drawing.Size(65, 19);
+            this.terminalToolStripMenuItem.Text = "Terminal";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            // 
+            // plotToolStripMenuItem
+            // 
+            this.plotToolStripMenuItem.Name = "plotToolStripMenuItem";
+            this.plotToolStripMenuItem.Size = new System.Drawing.Size(40, 19);
+            this.plotToolStripMenuItem.Text = "Plot";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 19);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // portBox
+            // 
+            this.portBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.portBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.portBox.FormattingEnabled = true;
+            this.portBox.Location = new System.Drawing.Point(10, 28);
+            this.portBox.Margin = new System.Windows.Forms.Padding(10, 5, 5, 5);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(85, 21);
+            this.portBox.TabIndex = 1;
+            // 
+            // baudBox
+            // 
+            this.baudBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.baudBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudBox.FormattingEnabled = true;
+            this.baudBox.Location = new System.Drawing.Point(105, 28);
+            this.baudBox.Margin = new System.Windows.Forms.Padding(5);
+            this.baudBox.Name = "baudBox";
+            this.baudBox.Size = new System.Drawing.Size(90, 21);
+            this.baudBox.TabIndex = 2;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.connectButton.Location = new System.Drawing.Point(314, 26);
+            this.connectButton.Margin = new System.Windows.Forms.Padding(5, 3, 10, 3);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(85, 24);
+            this.connectButton.TabIndex = 3;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // sendBox
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.sendBox, 3);
+            this.sendBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sendBox.FormattingEnabled = true;
+            this.sendBox.Location = new System.Drawing.Point(10, 281);
+            this.sendBox.Margin = new System.Windows.Forms.Padding(10, 5, 5, 3);
+            this.sendBox.Name = "sendBox";
+            this.sendBox.Size = new System.Drawing.Size(294, 21);
+            this.sendBox.TabIndex = 4;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sendButton.Location = new System.Drawing.Point(314, 279);
+            this.sendButton.Margin = new System.Windows.Forms.Padding(5, 3, 10, 3);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(85, 24);
+            this.sendButton.TabIndex = 5;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // receiveText
+            // 
+            this.receiveText.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.tableLayoutPanel1.SetColumnSpan(this.receiveText, 4);
+            this.receiveText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.receiveText.Location = new System.Drawing.Point(10, 56);
+            this.receiveText.Margin = new System.Windows.Forms.Padding(10, 3, 10, 3);
+            this.receiveText.Multiline = true;
+            this.receiveText.Name = "receiveText";
+            this.receiveText.ReadOnly = true;
+            this.receiveText.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.receiveText.Size = new System.Drawing.Size(389, 217);
+            this.receiveText.TabIndex = 6;
+            // 
+            // MonitorWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 311);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MonitorWindow";
             this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem terminalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem plotToolStripMenuItem;
+        private System.Windows.Forms.ComboBox portBox;
+        private System.Windows.Forms.ComboBox baudBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.ComboBox sendBox;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox receiveText;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
     }
 }
 
--- Strawhat.net/Form1.cs
+++ Strawhat.net/Form1.cs
@@ -3,18 +3,178 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Management;
 
 namespace Strawhat.net
 {
-    public partial class Form1 : Form
+    public partial class MonitorWindow : Form
     {
-        public Form1()
+        SerialPort serialPort = new SerialPort();
+        ManagementEventWatcher watcher = new ManagementEventWatcher();
+        WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent");
+        BindingList<String> ports = new BindingList<string>();
+        BindingSource pSource = new BindingSource();
+        BindingList<string> sent = new BindingList<string>();
+        BindingSource sSource = new BindingSource();
+
+        int n = 0;
+
+        public MonitorWindow()
         {
             InitializeComponent();
+            
+            //Initiate baud rate list
+            List<int> baudRate = new List<int>(){ 9600, 19200, 115200, 384000 };
+            baudBox.DataSource = baudRate;
+            baudBox.SelectedIndex = 0;
+            
+            //Bind ports list
+            pSource.DataSource = ports;
+            portBox.DataSource = pSource;
+            ports_refresh();
+
+            //Set receive text box empy
+            receiveText.Text = "";
+            
+            //Bind sendbox list
+            sSource.DataSource = sent;
+            sendBox.DataSource = sSource;
+
+            //Serial port
+            serialPort.Parity = Parity.None;
+            serialPort.StopBits = StopBits.One;
+            serialPort.DataBits = 8;
+            serialPort.Handshake = Handshake.None;
+            serialPort.RtsEnable = false;
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            //Set watcher event handler of usb device change event
+            watcher.EventArrived += new EventArrivedEventHandler(watcherEventArrived);
+            watcher.Query = query;
+            try
+            {
+                watcher.Start();
+            }
+            catch
+            {
+                Console.WriteLine("Watcher failed to start");
+            }
+        }
+
+        private void ports_refresh()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(delegate
+                {
+                    ports.Clear();
+                    Console.WriteLine("Ports cleared");
+                    foreach (String port in SerialPort.GetPortNames())
+                    {
+                        ports.Add(port);
+                        Console.Write(port);
+                        Console.WriteLine(" added");
+                    }
+                    ports.ResetBindings();
+                    Console.WriteLine("Ports binding resetted");
+                }));
+            }
+            else
+            {
+                ports.Clear();
+                Console.WriteLine("Ports cleared");
+                foreach (String port in SerialPort.GetPortNames())
+                {
+                    ports.Add(port);
+                    Console.Write(port);
+                    Console.WriteLine(" added");
+                }
+                ports.ResetBindings();
+                Console.WriteLine("Ports binding resetted");
+            }
+            
+        }
+
+        private void watcherEventArrived(Object sender, EventArrivedEventArgs e)
+        {
+            ports_refresh();
+            Console.WriteLine("Device change event");
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.PortName = portBox.SelectedItem.ToString();
+                    serialPort.BaudRate = Int32.Parse(baudBox.SelectedItem.ToString());
+                    serialPort.Open();
+                    connectButton.Text = "Disconnect";
+                    Console.WriteLine(serialPort.PortName + " " + serialPort.BaudRate + " Connected");
+                }
+                else if (serialPort.IsOpen)
+                {
+                    serialPort.DiscardInBuffer();
+                    serialPort.DiscardOutBuffer();
+                    serialPort.Close();
+                    connectButton.Text = "Connect";
+                    Console.WriteLine(serialPort.PortName + " " + serialPort.BaudRate + " Disconnected");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Connection Failed");
+            }
+        }
+
+        private void DataReceivedHandler(Object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            try
+            {
+                String data;
+                while (serialPort.BytesToRead > 0)
+                {
+                    data = sp.ReadExisting();
+                    if (receiveText.InvokeRequired)
+                    {
+                        receiveText.BeginInvoke(new MethodInvoker(delegate
+                        {
+                            receiveText.AppendText(data);
+                            receiveText.ScrollToCaret();
+                        }));
+                    }
+                }
+            }
+            catch (System.InvalidOperationException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            String sendMessage = sendBox.Text;
+            try
+            {
+                if (serialPort.IsOpen)
+                {
+                    serialPort.Write(sendMessage);
+                    sent.Add(sendMessage);
+                    sent.ResetBindings();
+                    sendBox.Text = "";
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Failed to send");
+            }
         }
     }
 }
